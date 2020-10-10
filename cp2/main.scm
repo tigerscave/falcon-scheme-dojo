7fc9a5da64969719f1fc701f8e1dbf38bf8fc18d
hikaru <hk924st@gmail.com>
2020-10-10 14:19:47 +0900

    write lat?

--- cp2/main.scm
+++ cp2/main.scm
@@ -1,3 +1,19 @@
 (print "hello world")
 
 (print (eq? 2 2))
+
+; atom? lat?
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond 
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+
+(print (atom? 'a))
+(print (lat? '(a b c (a))))
