dec2e6ae13a34ea2122246888b71fad7074384a7
falconkun <work8k@yahoo.co.jp>
2020-10-14 19:26:57 +0900

    kadai

--- cp2/main.scm
+++ cp2/main.scm
@@ -15,5 +15,16 @@
       ((atom? (car l)) (lat? (cdr l)))
       (else #f))))
 
+(define member?
+  (lambda (a l)
+    (cond
+      ((null? l) #f)
+      ((eq? a (car l)) #t)
+      (else (member? a (cdr l))))))
+
 (print (atom? 'a))
 (print (lat? '(a b c (a))))
+
+(print "--member--")
+(print (member? 'a '(a b c)))
+(print (member? 'a '(b c d a)))
